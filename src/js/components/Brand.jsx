2333f817467b13b310e4c471d52d941dec7fbae4
Brandon Giesing <me@brandongiesing.com>
2019-04-18 12:43:26 -0500

    Update Brand.jsx

--- src/js/components/Brand.jsx
+++ src/js/components/Brand.jsx
@@ -20,7 +20,7 @@ class Brand extends Component {
         {this.props.data.custom_name ? (
           <h1>{this.props.data.custom_name}</h1>
         ) : (
-          <img src='https://i.imgur.com/eiLI546.png' alt='MrSir2552' style={height: 1.7em;margin-top: 15px;}/>
+          <img src='https://i.imgur.com/eiLI546.png' alt='MrSir2552' style={height: 1.7 + 'em';margin-top: 15 + 'px';}/>
         )}
         {this.props.stream.stream ? <Status {...this.props} /> : <span />}
       </Branding>
